87676ee54921065640ec4a24f95e8bbda3c94d03
BlackMProgrammer <cristhiancastro01@gmail.com>
2020-09-09 09:21:01 -0400

    Add project files.

---
+++ SumaNumerosRandom/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Xml.Schema;
+
+namespace SumaNumerosRandom
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            bool valorr;
+            string valnumero;
+            double numero;
+            double total1;
+
+            Console.WriteLine("Desea Sumar un numero ? y/n");
+            string yon = Console.ReadLine();
+            total1 = 0;
+            if (yon == "y")
+            {
+                do
+                {
+                    Console.WriteLine("Digite un numero");
+                    valnumero = Console.ReadLine();
+                    numero = Convert.ToDouble(valnumero);
+
+                    if (numero != 0)
+                    {
+                        total1 = total1 + numero;
+                        Console.WriteLine("La suma es de {0}", total1);
+                    }
+
+                } while (numero != 0);
+            }
+            else {
+                Console.WriteLine("Intentelo nuevamente");
+            }
+            
+        }
+    }
+}
